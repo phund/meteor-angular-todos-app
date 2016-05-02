34e2d1cdef33186ca00cc5285ce4bfba5fe3226f
phund <itghost90@gmail.com>
2016-05-02 16:38:12 +0700

    Completed tasks

--- client/main.js
+++ client/main.js
@@ -1,22 +1,20 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
+ 
+angular.module('simple-todos', [
+  angularMeteor,
+  todosList.name,
+  'accounts.ui'
+]);
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+function onReady() {
+  angular.bootstrap(document, ['simple-todos']);
+}
+ 
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
